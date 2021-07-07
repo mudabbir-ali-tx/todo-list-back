07b72686f7d0057e89bcff83608627cbf91bcb57
mudabbir-ali-tx <mudabbir.ali@techcreatix.com>
2021-07-07 12:41:36 +0500

    make it better

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  # GET /todos
+  def index
+    @todos = Todo.all
+
+    render json: @todos
+  end
+
+  # GET /todos/1
+  def show
+    render json: @todo
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created, location: api_v1_todos_path(@todo)     
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /todos/1
+  def update
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /todos/1
+  def destroy
+    @todo.destroy
+      render json: {message: "User deleted successfully",item: @todo}
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def todo_params
+      params.require(:todo).permit(:task , :body)
+    end
+end
